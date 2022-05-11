9216996dc06c45aa03faae1fcc415d69444fc38a
root <root@LAPTOP-LMJQULD0.localdomain>
2022-05-11 14:33:44 +0100

    Officer Model defined

---
+++ api/models/officer.model.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize')
+const sequelize = require('../database')
+
+const Officers = sequelize.define('officers', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  fullName: {
+    type: DataTypes.STRING
+  },
+  department: {
+    type: DataTypes.INTEGER
+  },
+  currentCase: {
+    type: DataTypes.INTEGER
+  },
+  solvedCases: {
+    type: DataTypes.ARRAY(DataTypes.INTEGER)
+  }
+})
+
+module.exports = Officers
