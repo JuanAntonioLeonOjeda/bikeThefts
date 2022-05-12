1460210eaf398deb29ec392649d1eb9de0754c19
root <root@LAPTOP-LMJQULD0.localdomain>
2022-05-12 20:43:00 +0100

    Routers updated - All routers connected

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,3 +1,16 @@
 const router = require('express').Router()
 
+const authRouter = require('./auth.router')
+const userRouter = require('./user.router')
+const bikeRouter = require('./bike.router')
+const caseRouter = require('./case.router')
+const departmentRouter = require('./department.router')
+
+router
+  .use('/auth', authRouter)
+  .use('/user', userRouter)
+  .use('/bike', bikeRouter)
+  .use('/case', caseRouter)
+  .use('/department', departmentRouter)
+
 module.exports = router
