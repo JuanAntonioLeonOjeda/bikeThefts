7e45aab1be0efe56481e600b8ccfedcb71fa545e
root <root@LAPTOP-LMJQULD0.localdomain>
2022-05-13 09:54:30 +0100

    user signup and login functions created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,2 +1,43 @@
 const User = require('../models/user.model')
 
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+async function signup (req, res) {
+  try {
+    const hash = bcrypt.hashSync(req.body.password, 10)
+    req.body.password = hash
+
+    const user = await User.create(req.body)
+    const payload = { email: user.email }
+    const token = jwt.sign(payload, process.env.TOKEN, { expiresIn: '1h' })
+
+    res.status(200).json({ message: `${user.fullName}'s profile succesfully created`, info: { email: user.email, token: token } })
+  } catch (error) {
+    res.status(500).send(`Error creating user: ${error}`)
+  }
+}
+
+async function login (req, res) {
+  try {
+    const user = await User.findOne({ where: { email: req.body.email } })
+    if (!user) return res.status(500).send('Email or password incorrect')
+
+    bcrypt.compare(req.body.password, user.password, (err, result) => {
+      if (err) return res.status(500).send(err)
+      if (!result) return res.status('Email or password incorrect')
+
+      const payload = { email: user.email }
+      const token = jwt.sign(payload, process.env.TOKEN, { expiresIn: '1h' })
+
+      res.status(200).json({ email: user.email, token: token })
+    })
+  } catch (error) {
+    res.status(500).send('Error login user')
+  }
+}
+
+module.exports = {
+  signup,
+  login
+}
